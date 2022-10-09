8e39af10232f7ff2ec1427adafc55a59597dce92
Ruslan Amirov <gumagereiro@rambler.ru>
2022-10-09 14:07:38 +0300

    change_master_logic

--- app/src/main/java/com/example/test_git_1/MainActivity.kt
+++ app/src/main/java/com/example/test_git_1/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val a = 111
+        val b = 222
+        val c = 333
     }
 }
